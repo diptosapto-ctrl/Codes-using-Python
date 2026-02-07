afe50c6bfc9d7c717962acb427069f4fa7d4d635
diptosapto-ctrl <diptosapto@gmail.com>
2026-02-07 13:08:42 +0530

    Add files via upload

---
+++ File_creating.py
@@ -0,0 +1,6 @@
+#create , open , close a file..
+
+
+file=open("file1.txt" , 'xt')
+file.write("We are creating a new file using 'x' function \n We afe trying to learn file handling in python")
+file.close()
