d4af2213efdd3bf7df00f840d5a517e00cf38f35
diptosapto-ctrl <diptosapto@gmail.com>
2026-02-07 13:35:11 +0530

    Add files via upload

---
+++ Greeting text.py
@@ -0,0 +1,9 @@
+# 1.  Takes a user's first name and last name as input.
+# 2.  Concatenates the first name and last name into a full name.
+# 3.  Prints a personalized greeting message using the full name.
+
+
+first=input("Enter your first name:")
+last=input("Enter your last name:")
+
+print("Hallo!!",first+" "+last,"Welcome to the Python program")
